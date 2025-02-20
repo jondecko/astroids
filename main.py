9890c1d2ceae725e047d2c1a0c6d113f9c40a5e7
jondecko <jondecko@gmail.com>
2025-02-20 23:50:07 +0000

    first meaningful commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+
+def handle_exit_signal():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Our main game loop boys and girls!!!
+    continue_game_loop = True
+    while continue_game_loop:
+        handle_exit_signal()
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
