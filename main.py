5058b6adb6b55188e08dd754d7ea09ed44b86520
jondecko <jondecko@gmail.com>
2025-02-21 20:07:53 +0000

    use containers to manage the player

--- main.py
+++ main.py
@@ -6,32 +6,42 @@ from player import Player
 from constants import *
 
 
-def main():
-    pygame.init()
+def console_logging_startup_message():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+def main():
+    pygame.init()
+    console_logging_startup_message()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     player_start_x = SCREEN_WIDTH / 2
     player_start_y = SCREEN_HEIGHT / 2
     player = Player(player_start_x, player_start_y)
 
+
     # Our main game loop boys and girls!!!
     continue_game_loop = True
     while continue_game_loop:
-        # Handle an exit/close window signal
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")        
-        player.update(dt)
-        player.draw(screen)
+
+        for thing in updateable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
 
-        # limit the frames to 60 fps
         dt = clock.tick(60) / 1000
 
 
